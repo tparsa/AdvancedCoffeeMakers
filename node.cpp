b113e69e1a954c1b54679e605a2c31d808a624da
tparsa <parsa_ghorbani@outlook.com>
2019-01-25 16:38:48 +0330

    add node classes and methods

---
+++ node.cpp
@@ -0,0 +1,43 @@
+#include "node.hpp"
+
+std::map<Node*, bool> Node::fill_mark_for_validation(std::vector<Node*> available_children){
+    std::map<Node*, bool> mark;
+    for (Node* node : available_children)
+        mark[node] = true;
+    return mark;
+}
+
+bool Node::check_all_existence(std::vector<Node*> available_children)
+{
+    for (Node* node : available_children)
+        if(!exists_in_children(node))
+            return false;
+    return true;
+}
+
+bool Normal::validation(std::vector<Node*> available_children){
+    std::map<Node*, bool> mark;
+    mark = fill_mark_for_validation(available_children);
+    if(!check_all_existence(available_children))
+        return false;
+
+    for (Node* node : children)
+        if(node->get_optional() && mark[node] == false)
+            return false;
+    return true;
+}
+
+bool Or::validation(std::vector<Node*> available_children){
+    std::map<Node*, bool> mark;
+    mark = fill_mark_for_validation(available_children);
+    if(!check_all_existence(available_children))
+        return false;
+
+    return ((int)available_children.size() > 0);
+}
+
+bool Alternative::validation(std::vector<Node*> available_children){
+    if(!check_all_existence(available_children))
+        return false;
+    return ((int)available_children.size() == 1);
+}
---
+++ node.hpp
@@ -0,0 +1,34 @@
+#include <set>
+#include <vector>
+#include <map>
+#include <string>
+
+class Node {
+    protected:
+        int number_of_children;
+        std::set<Node*> children;
+        Node* parent;
+        std::string name;
+        bool optional;
+        bool exists_in_children(Node* node){ return !(children.find(node) == children.end()); }
+        std::map<Node*, bool> fill_mark_for_validation(std::vector<Node*> available_children);
+        bool check_all_existence(std::vector<Node*> available_children);
+    public:
+        bool get_optional() { return optional; }
+        virtual bool validation(std::vector<Node*> available_children) = 0;
+};
+
+class Normal: public Node  {
+    public:
+        bool validation(std::vector<Node*> available_children);
+};
+
+class Or: public Node {
+    public:
+        bool validation(std::vector<Node*> available_children);
+};
+
+class Alternative: public Node{
+    public:
+        bool validation(std::vector<Node*> available_children);
+};
