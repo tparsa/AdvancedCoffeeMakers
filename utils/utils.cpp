80ae9c32af3ed5edb9c6d18e5b7cb1fa5ba54fee
tparsa <parsa_ghorbani@outlook.com>
2019-01-25 15:38:01 +0330

    add split to utils

---
+++ utils/utils.cpp
@@ -0,0 +1,15 @@
+#include "utils.hpp"
+
+std::vector<std::string> split (std::string str){
+    std::vector<std::string> ret;
+
+    int str_size = (int)str.size();
+    std::string curr_part = "";
+    for (int i = 0; i < str_size; i++){
+        if(str[i] == ' '){
+            if((int)curr_part.size() != 0)
+                ret.push_back(curr_part);
+            curr_part = "";
+        }
+    }
+}
