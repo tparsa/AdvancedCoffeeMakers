639208bd2ee4c15c6e1132c1ff9103db06fb0573
tparsa <parsa_ghorbani@outlook.com>
2019-01-26 19:45:44 +0330

    add space eliminator that would fix a major problem

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -1,15 +1,26 @@
 #include "utils.hpp"
 
+#define SPACE 32
+#define TAB 9
+
+std::string eliminate_white_spaces(std::string str){
+    std::string ret;
+    for (int i = 0; i < (int)str.size(); i++)
+        if((int)str[i] != SPACE && (int)str[i] != TAB)
+            ret += str[i];
+    return ret;
+}
+
 std::vector<std::string> split (std::string str){
     std::vector<std::string> ret;
-
+    str = eliminate_white_spaces(str);
     int str_size = (int)str.size();
     std::string curr_part = "";
     for (int i = 0; i < str_size; i++){
         if(!isalpha(str[i]) && str[i] != '?'){
             if((int)curr_part.size() != 0)
                 ret.push_back(curr_part);
-            if(str[i] != ' ' && int(str[i]) != 9){
+            if((int)str[i] != SPACE && int(str[i]) != TAB){
                 curr_part = str[i];
                 ret.push_back(curr_part);
             }
